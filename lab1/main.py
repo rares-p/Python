55488ca6ee69c9615ba67c06e56d43e91ed11893
rares-p <panainte.rares02@gmail.com>
2023-10-17 00:38:56 +0300

    fara hi_name

--- lab1/main.py
+++ lab1/main.py
@@ -1,11 +1,6 @@
 import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Find The greatest common divisor of multiple numbers read from the console.
 def ex1():
     numbers = [int(x) for x in input().split()]
