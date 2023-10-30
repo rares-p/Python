ca7ad1301927e241e2dd8afb855a515f9b04c24e
rares-p <panainte.rares02@gmail.com>
2023-10-30 17:03:26 +0200

    ;)

--- lab3/main.py
+++ lab3/main.py
@@ -34,9 +34,7 @@ def ex3(a, b):
 
 
 # The build_xml_element function receives the following parameters: tag, content, and key-value elements given as
-# name-parameters. Build and return a string that represents the corresponding XML element. Example:
-# build_xml_element ("a", "Hello there", href =" http://python.org ", _class =" my-link ", id= " someid ") returns
-# the string = "<a href="http://python.org \ "_class = " my-link \ "id = " someid \ "> Hello there "
+# name-parameters. Build and return a string that represents the corresponding XML element.
 def ex4(tag, content, **kwargs):
     result = f'<{tag.strip()}'
     for key, value in kwargs.items():
