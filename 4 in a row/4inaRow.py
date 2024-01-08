cca23882d5f0ab4c64c0a378b4b9bd92b0cd73ac
rares-p <panainte.rares02@gmail.com>
2024-01-08 23:00:19 +0200

    pus linia la 79 daca asa zice pep

--- 4 in a row/4inaRow.py
+++ 4 in a row/4inaRow.py
@@ -12,14 +12,20 @@ YELLOW = (242, 245, 66)
 def read_input(args):
     size = len(args)
     if size < 4 or size > 5:
-        print("Incorrect number of arguments. Correct usage is: 4inaROW.py <opponent type> <board width> "
-              "<board height> [firstPlayer]\nEX: 4inaROW.py computer2 7 5 human\nEX: 4inaROW.py human 7 5\nOpponent "
-              "types are: human/computer1/computer2/computer3 (where 1 2 3 are difficulties in ascending order)")
+        print(
+            "Incorrect number of arguments. Correct usage is: 4inaROW.py "
+            "<opponent type> <board width>"
+            "<board height> [firstPlayer]\nEX: 4inaROW.py computer2 7 5 "
+            "human\nEX: 4inaROW.py human 7 5\nOpponent"
+            "types are: human/computer1/computer2/computer3 (where 1 2 3 are "
+            "difficulties in ascending order)")
         exit(-1)
     opponent_type = args[1]
     if opponent_type not in ["human", "computer1", "computer2", "computer3"]:
-        print("Incorrect opponent type. Available types are: human/computer1/computer2/computer3 (where 1 2 3 are "
-              "difficulties in ascending order)")
+        print(
+            "Incorrect opponent type. Available types are: "
+            "human/computer1/computer2/computer3 (where 1 2 3 are"
+            "difficulties in ascending order)")
         exit(-1)
     width = int(args[2])
     height = int(args[3])
@@ -32,7 +38,9 @@ def read_input(args):
     first_player = None
     if opponent_type == "human":
         if size == 5:
-            print("First player specification is redundant since the opponent is human.\n")
+            print(
+                "First player specification is redundant since the opponent "
+                "is human.\n")
     elif size == 5:
         first_player = args[4]
         if first_player not in ["human", "computer"]:
@@ -43,8 +51,10 @@ def read_input(args):
 
 
 class FourInARow:
-    def __init__(self, opponent_type: str, width: int, height: int, first_player: str):
-        self.opponent_type = "human" if opponent_type == "human" else "computer"
+    def __init__(self, opponent_type: str, width: int, height: int,
+                 first_player: str):
+        self.opponent_type = "human" if opponent_type == "human" \
+            else "computer"
         self.computer = False
         self.screen = None
         self.font = None
@@ -62,22 +72,25 @@ class FourInARow:
                 self.first_player = "computer"
         self.window_header = self.header * self.window_scale
         self.window_width = self.width * self.window_scale
-        self.window_height = self.height * self.window_scale + self.window_header
+        self.window_height = (self.height * self.window_scale
+                              + self.window_header)
         self.initial_setup()
 
     def initial_setup(self):
         pygame.init()
         pygame.display.set_caption("4 in a row")
         self.font = pygame.font.SysFont("Comic Sans MS", 36)
-        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
+        self.screen = pygame.display.set_mode(
+            (self.window_width, self.window_height))
         self.screen.fill(BLUE)
-        pygame.draw.rect(self.screen, BEIGE, (0, 0, self.window_width, self.window_header))
+        pygame.draw.rect(self.screen, BEIGE,
+                         (0, 0, self.window_width, self.window_header))
         for i in range(self.width):
             for j in range(self.header, self.height + self.header):
                 pygame.draw.circle(self.screen, BEIGE, (
                     i * self.window_scale + self.window_scale // 2, j *
                     self.window_scale + self.window_scale // 2),
-                    self.window_scale / 2 - self.margin)
+                                   self.window_scale / 2 - self.margin)
         pygame.display.update()
 
     def move(self, x, opp):
@@ -92,8 +105,9 @@ class FourInARow:
         color = YELLOW if opp == 0 else RED
         pygame.draw.circle(self.screen, color, (
             x * self.window_scale + self.window_scale // 2,
-            y * self.window_scale + self.window_scale // 2 + self.window_header),
-                           self.window_scale / 2 - self.margin)
+            y * self.window_scale + self.window_scale // 2 +
+            self.window_header),
+            self.window_scale / 2 - self.margin)
         pygame.display.update()
 
     def update_turn_text(self, opp):
@@ -109,7 +123,10 @@ class FourInARow:
         pygame.display.update()
 
     def display_text(self, display_text, color):
-        pygame.draw.rect(self.screen, BEIGE, (0, 0, self.window_width, self.window_header))
+        pygame.draw.rect(self.screen, BEIGE, (0,
+                                              0,
+                                              self.window_width,
+                                              self.window_header))
         text = self.font.render(display_text, True, color, (10, 10, 10))
         textRect = text.get_rect()
         textRect.center = (self.window_width // 2, self.window_header // 2)
@@ -149,7 +166,8 @@ class FourInARow:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN and not over:
                     if event.button == 1:
-                        if self.move(event.pos[0] // self.window_scale, turn) != -1:
+                        if (self.move(event.pos[0] // self.window_scale, turn)
+                                != -1):
                             turn = 1 - turn
                             if self.check_game_over(turn):
                                 over = True
